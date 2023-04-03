1b14ada7c8039f931fab65ae3b45e5c9275cb1ff
LordLion0000 <lordlion.ro@gmail.com>
2023-04-03 17:39:29 +0300

    new file:   coderun3_logo.png 	modified:   index.html 	modified:   indexCSS.css 	new file:   indexJS.js 	new file:   logoCR-white.png 	new file:   poza1.png

---
+++ indexJS.js
@@ -0,0 +1,24 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes'); 
+const seconds = document.getElementById('seconds');
+
+
+const coderun = new Date('April 18 2023 10:00:00');
+
+function update_countdown_time()
+{
+    const time_now = new Date();
+    const diff = coderun - time_now;
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60 ) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(update_countdown_time, 1000)
+
